551b0ac80680620bbafc752acf78dce828a59f75
Lucas Souza <lucas-sdf@hotmail.com.br>
2020-06-02 00:40:08 -0300

    tela Selecao de pagamento

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -4,7 +4,7 @@ import Background from '../../components/Background';
 import {
   Container,
   ProcessPayment,
-  AddressContainer,
+  ItemScrollContainer,
   Street,
   City,
   Cep,
@@ -13,6 +13,32 @@ import {
   AddAddressesButtonText,
   UpdateAddressesButton,
   UpdateAddressesButtonText,
+  TitleCartContainer,
+  TitleCartValidation,
+  CartContainer,
+  DataCartContainer,
+  ItemCartName,
+  PriceProductsCart,
+  TotalPriceSale,
+  ButtonSelectPayment,
+  ButtonContainer,
+  PaymentsContainer,
+  CreditContainer,
+  TextCardData,
+  DateCardContainer,
+  ButtonCardData,
+  ButtonFinalizeSale,
+  ButtonSaveCard,
+  TextFinalizeSale,
+  TextCardTitle,
+  TextIconName,
+  TextSelectPayment,
+  TitlePaymentContainer,
+  ButtonSelectCard,
+  BankSlipContainer,
+  PrinterContainer,
+  IconContainer,
+  TextBankSlipDescription,
 } from './styles';
 
 const Payment = () => {
@@ -20,7 +46,7 @@ const Payment = () => {
     <Background>
       <Container>
         <ProcessPayment>
-          <AddressContainer>
+          <ItemScrollContainer>
             <Street>Rua 1, 350</Street>
             <City>São Paulo - SP</City>
             <Cep>05847-120</Cep>
@@ -36,24 +62,83 @@ const Payment = () => {
                 <AddAddressesButtonText>Add endereço</AddAddressesButtonText>
               </AddAddressesButton>
             </AddressButtonsContainer>
-          </AddressContainer>
-          <AddressContainer>
-            <Street>Rua 1, 350</Street>
-            <City>São Paulo - SP</City>
-            <Cep>05847-120</Cep>
-            <AddressButtonsContainer>
-              <UpdateAddressesButton>
-                <Icon name="home" size={20} color="#000" />
-                <UpdateAddressesButtonText>
-                  Alterar endereço
-                </UpdateAddressesButtonText>
-              </UpdateAddressesButton>
-              <AddAddressesButton>
-                <Icon name="add" size={20} color="#000" />
-                <AddAddressesButtonText>Add endereço</AddAddressesButtonText>
-              </AddAddressesButton>
-            </AddressButtonsContainer>
-          </AddressContainer>
+          </ItemScrollContainer>
+
+          <ItemScrollContainer>
+            <TitleCartContainer>
+              <TitleCartValidation>Resumo do pedido</TitleCartValidation>
+            </TitleCartContainer>
+            <CartContainer>
+              <DataCartContainer>
+                <ItemCartName>1 Produto visualizar</ItemCartName>
+                <PriceProductsCart>R$85,10</PriceProductsCart>
+              </DataCartContainer>
+              <DataCartContainer>
+                <ItemCartName>Frete</ItemCartName>
+                <PriceProductsCart>R$0,10</PriceProductsCart>
+              </DataCartContainer>
+            </CartContainer>
+            <TotalPriceSale>R$85,20</TotalPriceSale>
+          </ItemScrollContainer>
+
+          <ItemScrollContainer>
+            <ButtonContainer>
+              <ButtonSelectPayment>
+                <Icon name="credit-card" size={80} color="#000" />
+              </ButtonSelectPayment>
+              <ButtonSelectPayment>
+                <Icon name="attach-money" size={80} color="#000" />
+              </ButtonSelectPayment>
+            </ButtonContainer>
+
+            <TextSelectPayment>
+              <TextIconName>Cartão de crédito</TextIconName>
+              <TextIconName>Boleto</TextIconName>
+            </TextSelectPayment>
+
+            <PaymentsContainer>
+              <CreditContainer>
+                <TitlePaymentContainer>
+                  <TextCardTitle>Escolher cartão</TextCardTitle>
+                  <ButtonSelectCard>
+                    <Icon name="open-in-new" size={20} color="#000" />
+                  </ButtonSelectCard>
+                </TitlePaymentContainer>
+                <TextCardData>Nome Impresso no cartão</TextCardData>
+                <TextCardData>Número do cartão</TextCardData>
+                <DateCardContainer>
+                  <ButtonCardData>Mês</ButtonCardData>
+                  <ButtonCardData>Ano</ButtonCardData>
+                </DateCardContainer>
+                <TextCardData>CVV</TextCardData>
+                <TextCardData>Parcelar em</TextCardData>
+                <ButtonSaveCard>
+                  <TextFinalizeSale>Selecione para salvar seu cartão</TextFinalizeSale>
+                </ButtonSaveCard>
+                <ButtonFinalizeSale>
+                  <TextFinalizeSale>Pagar com cartão</TextFinalizeSale>
+                </ButtonFinalizeSale>
+              </CreditContainer>
+
+              <BankSlipContainer>
+                <PrinterContainer>
+                  <IconContainer>
+                    <Icon name="print" size={50} color="#000"/>
+                    <TextBankSlipDescription>Imprima o Boleto e pague no banco</TextBankSlipDescription>
+                  </IconContainer>
+                  <IconContainer>
+                    <Icon name="computer" size={50} color="#000"/>
+                    <TextBankSlipDescription>ou pague pela internet utilizando
+                        o código de barras do boleto.</TextBankSlipDescription>
+                  </IconContainer>
+                  <IconContainer>
+                    <Icon name="event" size={50} color="#000"/>
+                    <TextBankSlipDescription>O prazo de validade do boleto é de um dia útil.</TextBankSlipDescription>
+                  </IconContainer>
+                </PrinterContainer>
+              </BankSlipContainer>
+            </PaymentsContainer>
+          </ItemScrollContainer>
         </ProcessPayment>
       </Container>
     </Background>
--- src/pages/Payment/styles.js
+++ src/pages/Payment/styles.js
@@ -8,7 +8,8 @@ export const ProcessPayment = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
 })``;
-export const AddressContainer = styled.View`
+export const ItemScrollContainer = styled.View`
+  width: 400px;
   background: #fff;
   padding: 20px;
   border-radius: 4px;
@@ -55,4 +56,116 @@ export const UpdateAddressesButton = styled.TouchableOpacity`
 export const UpdateAddressesButtonText = styled.Text`
   font-size: 16px;
   margin-left: 5px;
+`;
+
+export const TitleCartContainer = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const TitleCartValidation = styled.Text``;
+export const CartContainer = styled.View`
+  margin-top: 10px;
+  flex: 1;
+`;
+export const DataCartContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const ItemCartName = styled.Text`
+  font-size: 16px;
+  margin-right: 10px;
+`;
+export const PriceProductsCart = styled.Text`
+  font-size: 16px;
+`;
+export const TotalPriceSale = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  align-self: flex-end;
+`;
+export const ButtonSelectPayment = styled.TouchableOpacity`
+  margin: 0px 40px;
+`;
+
+export const TextIconName = styled.Text`
+  margin: 0px 60px 20px 20px;
+  font-size: 16px;
+`;
+
+export const TextSelectPayment = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+
+export const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+
+`;
+
+export const PaymentsContainer = styled.View`
+  margin-top: 20px;
+`;
+export const CreditContainer = styled.View`
+/*margin-right: 500px; ////////////////////////////////////////////////////////////POG */
+`;
+export const TextCardData = styled.TextInput`
+  font-size: 16px;
+  background-color: #E6E6E6;
+`;
+export const DateCardContainer = styled.View`
+  flex-direction: row;
+  background-color: #E6E6E6;
+  padding: 2px
+`;
+export const ButtonCardData = styled.TextInput`
+  margin-right: 160px;
+  font-size: 16px;
+`;
+
+export const ButtonSaveCard = styled.TouchableOpacity`
+  margin-top: 20px;
+  background: #de4e3a;
+  border-radius: 4px;
+  padding: 15px;
+`;
+
+export const ButtonFinalizeSale = styled.TouchableOpacity`
+  margin-top: 10px;
+  background: #de4e3a;
+  border-radius: 4px;
+  padding: 15px;
+`;
+
+export const TextFinalizeSale = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  align-self: center;
+`;
+
+export const TextCardTitle = styled.Text`
+  font-size: 16px;
+  margin-bottom: 10px;
+`;
+
+export const TitlePaymentContainer = styled.View`
+  flex-direction: row;
+`;
+export const ButtonSelectCard = styled.TouchableOpacity`
+`;
+
+export const BankSlipContainer = styled.View`
+  margin-right: 500px;/* ////////////////////////////////////////////////////////////POG */
+  padding: 0px 100px 0px 0px;
+`;
+
+export const PrinterContainer = styled.View`
+`;
+export const IconContainer = styled.View`
+  flex-direction: row;
+`;
+export const TextBankSlipDescription = styled.Text`
+  font-size: 16px;
+  margin-left: 10px;
 `;
